497e9b21b50caca9603ff4821abd8409b71aa94a
abhishekg495 <abhishekgupta23810306@gmail.com>
2022-03-19 01:52:46 +0530

    Uploaded all files

--- final/src/App.js
+++ final/src/App.js
@@ -1,47 +1,106 @@
 import React, { useState } from 'react';
 
-export default function App() {
-	const questions = [
-		{
-			questionText: 'What is the capital of France?',
-			answerOptions: [
-				{ answerText: 'New York', isCorrect: false },
-				{ answerText: 'London', isCorrect: false },
-				{ answerText: 'Paris', isCorrect: true },
-				{ answerText: 'Dublin', isCorrect: false },
-			],
-		},
-		{
-			questionText: 'Who is CEO of Tesla?',
-			answerOptions: [
-				{ answerText: 'Jeff Bezos', isCorrect: false },
-				{ answerText: 'Elon Musk', isCorrect: true },
-				{ answerText: 'Bill Gates', isCorrect: false },
-				{ answerText: 'Tony Stark', isCorrect: false },
-			],
-		},
-		{
-			questionText: 'The iPhone was created by which company?',
-			answerOptions: [
-				{ answerText: 'Apple', isCorrect: true },
-				{ answerText: 'Intel', isCorrect: false },
-				{ answerText: 'Amazon', isCorrect: false },
-				{ answerText: 'Microsoft', isCorrect: false },
-			],
-		},
-		{
-			questionText: 'How many Harry Potter books are there?',
-			answerOptions: [
-				{ answerText: '1', isCorrect: false },
-				{ answerText: '4', isCorrect: false },
-				{ answerText: '6', isCorrect: false },
-				{ answerText: '7', isCorrect: true },
-			],
-		},
-	];
 
+function CreateQuiz(props) {
+	const questionsList = props.questions.map((ques, i) =>
+		<div className='question' key={i}>
+			<span>{i + 1 + ":"} {ques.questionText}</span>
+		</div>
+	);
+	const [ques, setQuestion] = useState('')
+	const [option1, updateOption1] = useState('')
+	const [option1Correct, updateOption1Correct] = useState('')
+	const [option2, updateOption2] = useState('')
+	const [option2Correct, updateOption2Correct] = useState('')
+	const [option3, updateOption3] = useState('')
+	const [option3Correct, updateOption3Correct] = useState('')
+	const [option4, updateOption4] = useState('')
+	const [option4Correct, updateOption4Correct] = useState('')
+	function handleSubmit(e) {
+		e.preventDefault()
+		props.setShowQuestions(prev => true)
+	}
+
+	function addQuestion(e) {
+		e.preventDefault()
+		props.updateQuestions(prev => prev.concat({
+			questionText: ques,
+			answerOptions: [
+				{ answerText: option1, isCorrect: option1Correct === 'Yes' },
+				{ answerText: option2, isCorrect: option2Correct === 'Yes' },
+				{ answerText: option3, isCorrect: option3Correct === 'Yes' },
+				{ answerText: option4, isCorrect: option4Correct === 'Yes' },
+			],
+		}))
+		setQuestion('')
+		updateOption1('')
+		updateOption1Correct('No')
+		updateOption2('')
+		updateOption2Correct('No')
+		updateOption3('')
+		updateOption3Correct('No')
+		updateOption4('')
+		updateOption4Correct('No')
+		console.log(props.questions)
+	}
+	return (
+		<div className='create-questions'>
+			{questionsList}
+			< form >
+				<fieldset className='question-input'>
+					<input value={ques} onChange={e => setQuestion(e.target.value)} placeholder="Question" />
+					<fieldset className='option-input'>
+						<input value={option1} onChange={e => updateOption1(e.target.value)} placeholder="Option 1" required />
+						<select vale={option1Correct} onChange={e => updateOption1Correct(e.target.value)} placeholder='Is this one of the answers ?'>
+							<option>Yes</option>
+							<option>No</option>
+						</select>
+					</fieldset>
+					<fieldset className='option-input'>
+						<input value={option2} onChange={e => updateOption2(e.target.value)} placeholder="Option 2" required />
+						<select vale={option2Correct} onChange={e => updateOption2Correct(e.target.value)} placeholder='Is this one of the answers ?'>
+							<option>Yes</option>
+							<option>No</option>
+						</select>
+					</fieldset>
+					<fieldset className='option-input'>
+						<input value={option3} onChange={e => updateOption3(e.target.value)} placeholder="Option 3" required />
+						<select vale={option3Correct} onChange={e => updateOption3Correct(e.target.value)} placeholder='Is this one of the answers ?'>
+							<option>Yes</option>
+							<option>No</option>
+						</select>
+					</fieldset>
+					<fieldset className='option-input'>
+						<input value={option4} onChange={e => updateOption4(e.target.value)} placeholder="Option 4" required />
+						<select vale={option4Correct} onChange={e => updateOption4Correct(e.target.value)} placeholder='Is this one of the answers ?'>
+							<option>Yes</option>
+							<option>No</option>
+						</select>
+					</fieldset>
+					<button onClick={addQuestion} className="question-button"> Add Question </button>
+					<button onClick={handleSubmit}> Done </button>
+				</fieldset>
+			</form >
+		</div>
+	)
+
+}
+export default function App() {
+	// const questions = [
+	// 	{
+	// 		questionText: 'What is the capital of France?',
+	// 		answerOptions: [
+	// 			{ answerText: 'New York', isCorrect: false },
+	// 			{ answerText: 'London', isCorrect: false },
+	// 			{ answerText: 'Paris', isCorrect: true },
+	// 			{ answerText: 'Dublin', isCorrect: false },
+	// 		],
+	// 	}]
+
+	const [questions, updateQuestions] = useState([]);
 	const [currentQuestion, setCurrentQuestion] = useState(0);
 	const [showScore, setShowScore] = useState(false);
+	const [showQuestions, setShowQuestions] = useState(false);
 	const [score, setScore] = useState(0);
 
 	const handleAnswerOptionClick = (isCorrect) => {
@@ -63,19 +122,24 @@ export default function App() {
 					You scored {score} out of {questions.length}
 				</div>
 			) : (
-				<>
-					<div className='question-section'>
-						<div className='question-count'>
-							<span>Question {currentQuestion + 1}</span>/{questions.length}
+				showQuestions ? (
+					<>
+						<div className='question-section'>
+							<div className='question-count'>
+								<span>Question {currentQuestion + 1}</span>/{questions.length}
+							</div>
+							<div className='question-text'>{questions[currentQuestion].questionText}</div>
 						</div>
-						<div className='question-text'>{questions[currentQuestion].questionText}</div>
-					</div>
-					<div className='answer-section'>
-						{questions[currentQuestion].answerOptions.map((answerOption) => (
-							<button onClick={() => handleAnswerOptionClick(answerOption.isCorrect)}>{answerOption.answerText}</button>
-						))}
-					</div>
-				</>
+						<div className='answer-section'>
+							{questions[currentQuestion].answerOptions.map((answerOption) => (
+								<button onClick={() => handleAnswerOptionClick(answerOption.isCorrect)}>{answerOption.answerText}</button>
+							))}
+						</div>
+					</>
+				) : (
+					<CreateQuiz updateQuestions={updateQuestions} setShowQuestions={setShowQuestions} questions={questions} />
+				)
+
 			)}
 		</div>
 	);
